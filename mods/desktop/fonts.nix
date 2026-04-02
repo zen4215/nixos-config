cbf552b58c1150fa7c7e5a19d5742f0e7191c79e
zen4215 <44k1.373@gmail.com>
2026-04-02 18:05:59 +0900

    fix: fonts.nix

--- mods/desktop/fonts.nix
+++ mods/desktop/fonts.nix
@@ -1,22 +1,20 @@
+{ pkgs, ... }:
+
 {
-  { pkgs, ... }:
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      udev-gothic-nf
+    ];
 
-  {
-    fonts = {
-      packages = with pkgs; [
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        noto-fonts-color-emoji
-        udev-gothic-nf
-      ];
-
-      fontconfig = {
-        defaultFonts = {
-          serif = [ "Noto Serif CJK JP" "Noto Color Emoji" ];
-          sansSerif = [ "Noto Sans CJK JP" "Noto Color Emoji" ];
-          monospace = [ "UDEV Gothic Console NF" "Noto Color Emoji" ];
-        };
+   fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Serif CJK JP" "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans CJK JP" "Noto Color Emoji" ];
+        monospace = [ "UDEV Gothic Console NF" "Noto Color Emoji" ];
       };
     };
-  }
+  };
 }
